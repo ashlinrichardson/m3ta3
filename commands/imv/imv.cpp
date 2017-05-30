9b53662cfacc7ac51cc7b8a8db276ff4158c462d
ashlinrichardson <richardson.ashlin@gmail.com>
2017-05-30 13:41:22 -0700

    add header file reading
    add header file reading

--- commands/imv/imv.cpp
+++ commands/imv/imv.cpp
@@ -2,18 +2,7 @@
 #include "../../glut2/newzpr.h"
 #include<fstream>
 #include<iostream>
-
-long int getFileSize(std::string  fn){
-  ifstream i;
-  i.open(fn.c_str(), ios::binary);
-  if(!i.is_open()){
-    cout << "error: couldn't open file: " << fn << endl;
-    return (-1);
-  }
-  i.seekg(0,ios::end);
-  long int len = i.tellg();
-  return(len);
-}
+#include"util.h"
 
 int main(int argc, char * argv[]){
   if(sizeof(float) != 4){
@@ -21,22 +10,22 @@ int main(int argc, char * argv[]){
     exit(1);
   }
 
-	if(argc <5){
-		printf("imv.cpp: [infile] [nrow] [ncol] [nb]\n"); exit(1); 
+	if(argc < 2){
+		printf("imv.cpp: [infile]\n"); // [nrow] [ncol] [nb]\n");
+    exit(1); 
 	}
 
 	char * infile = argv[1];
-	long int NRow = (long int)atoi(argv[2]);
-	long int NCol = (long int)atoi(argv[3]);
-	long int NBand = (long int)atoi(argv[4]);
-	
+	long int NRow, NCol, NBand; 
+  parseHeaderFile(string(infile), NRow, NCol, NBand);
+
   printf(" infile: %s nrow %ld ncol %ld nband %ld\n", infile, NRow, NCol, NBand);
 	printf(" getFileSize %ld expected %ld\n", getFileSize(infile), NRow*NCol*NBand*sizeof(float));
   
   FILE * f = fopen(infile, "rb");
-	SA<float> dat(NRow*NCol*NBand);
-	long int nread = fread( &dat[0], sizeof(float), NRow*NCol*NBand, f);
-	if((nread)!=(NRow*NCol*NBand)){
+	SA<float> dat(NRow * NCol * NBand);
+	long int nread = fread(&dat[0], sizeof(float), NRow * NCol * NBand, f);
+	if(nread != NRow * NCol * NBand){
     printf("Error (imv.cpp): number of elements read (%d) different than expected (%d)\n", nread, NRow*NCol*NBand); 
     exit(1);
   }
@@ -46,7 +35,7 @@ int main(int argc, char * argv[]){
 	a.initFrom(&dat, NRow, NCol, NBand);
 	zprManager * myManager = zprManager::Instance(argc,argv);
 	zprInstance * myZpr = myManager->newZprInstance(NRow, NCol,NBand);
-	glImage * myImage = new glImage( myZpr, &a);
+	glImage * myImage = new glImage(myZpr, &a);
 	glutMainLoop();
 	return 0;
 }
