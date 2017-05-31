c2397608d6fdec22f61125920a3b8370eae92562
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2017-05-30 21:07:19 -0700

    Update imv.cpp

--- commands/imv/imv.cpp
+++ commands/imv/imv.cpp
@@ -9,31 +9,32 @@ int main(int argc, char * argv[]){
     printf("Error: sizeof(float) != 4\n");
     exit(1);
   }
-	if(argc < 2){
-		printf("imv.cpp: [infile]\n");
-    exit(1);
-	}
-	char * infile = argv[1];
-	long int NRow, NCol, NBand;
+	
+  if(argc < 2){
+    printf("imv.cpp: [infile]\n");
+   exit(1);
+  }
+  char * infile = argv[1];
+  long int NRow, NCol, NBand;
   parseHeaderFile(getHeaderFileName(string(infile)), NRow, NCol, NBand);
   printf(" infile: %s nrow %ld ncol %ld nband %ld\n", infile, NRow, NCol, NBand);
-	printf(" getFileSize %ld expected %ld\n", getFileSize(infile), NRow*NCol*NBand*sizeof(float));
+  printf(" getFileSize %ld expected %ld\n", getFileSize(infile), NRow*NCol*NBand*sizeof(float));
 
   FILE * f = fopen(infile, "rb");
-	SA<float> dat(NRow * NCol * NBand);
-	long int nread = fread(&dat[0], sizeof(float), NRow * NCol * NBand, f);
-	if(nread != NRow * NCol * NBand){
+  SA<float> dat(NRow * NCol * NBand);
+  long int nread = fread(&dat[0], sizeof(float), NRow * NCol * NBand, f);
+  if(nread != NRow * NCol * NBand){
     printf("Error (imv.cpp): number of elements read (%d) different than expected (%d)\n", nread, NRow*NCol*NBand);
     exit(1);
   }
-	fclose(f);
+  fclose(f);
 
   myImg a;
-	a.initFrom(&dat, NRow, NCol, NBand);
-	zprManager * myManager = zprManager::Instance(argc,argv);
-	zprInstance * myZpr = myManager->newZprInstance(NRow, NCol,NBand);
-	glImage * myImage = new glImage(myZpr, &a);
-	glutMainLoop();
-	return 0;
+  a.initFrom(&dat, NRow, NCol, NBand);
+  zprManager * myManager = zprManager::Instance(argc,argv);
+  zprInstance * myZpr = myManager->newZprInstance(NRow, NCol,NBand);
+  glImage * myImage = new glImage(myZpr, &a);
+  glutMainLoop();
+  return 0;
 }
 
